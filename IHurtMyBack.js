51d132cc4cd8ef6a41bc3a66748a991cd8dc8011
Head <mrdoo309@gmail.com>
2018-11-28 14:22:26 +0900

    input내의 value값을 가져오는 방법을 찾다가 실패..

---
+++ IHurtMyBack.js
@@ -0,0 +1,12 @@
+
+
+function checkTime() {
+    let time = document.getElementById('time').value
+    console.log(time)
+}
+
+
+window.addEventListener('load', function load(event){
+    var createButton = document.getElementById('startButton');
+    createButton.addEventListener('click', checkTime());
+})
