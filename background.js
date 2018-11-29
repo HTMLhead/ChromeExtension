195085450ab1bcc1cb085b4142502eafabc45079
Head <mrdoo309@gmail.com>
2018-11-29 15:02:31 +0900

    dominate

--- background.js
+++ background.js
@@ -8,7 +8,7 @@ chrome.alarms.onAlarm.addListener(function () {
         title: '스트레칭 할 시간입니다.',
         message: '쭉쭉쭉',
         buttons: [
-            { title: '뭐 어쩌라구요. 이건 왜누른건데요' }
+            { title: '더보기를 누른 이유를 생각하며 스트레칭 하세요' }
         ],
         priority: 0
     });
