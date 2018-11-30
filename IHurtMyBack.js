03e58c59c5a353a9cfa4f870ca401c48bf272e8d
Head <mrdoo309@gmail.com>
2018-11-30 11:41:11 +0900

    js파일 완성

--- IHurtMyBack.js
+++ IHurtMyBack.js
@@ -1,33 +1,38 @@
 'use strict';
-let getChangeTime
+let time
 
 function isNumber(letter) {
-    if(isNaN(letter)) {
-        
+    if (isNaN(letter)) {
+        return true
     }
+}
+
+function displayTime(minutesTimeData) {
+    time = setInterval(() => {
+        minutesTimeData = minutesTimeData - 1
+        if (minutesTimeData <= 0) {
+            clearInterval(time)
+        }
+        return chrome.browserAction.setBadgeText({ text: `${minutesTimeData}` })
+    }, 60000)
 }
 
 function setAlarm() {
     let minutes = Number(document.getElementById('time').value)
-    if(isNaN(minutes)) {
-        return alert('숫자만 입력해 주세요')
-    }
-    getChangeTime = setInterval(() => {
-        if(minutes <= 0) {
-            clearInterval(getChangeTime)
-        }
-        minutes = minutes - 1
-        return chrome.browserAction.setBadgeText({text: `${minutes}`})
-    }, 60000)
-    chrome.browserAction.setBadgeText({ text: `${minutes}` });
+    if (isNumber(minutes)) return alert('숫자만 입력해 주세요.')
+
+    displayTime(minutes)
+    chrome.browserAction.setBadgeText({ text: `${minutes}` })
     chrome.alarms.create({ delayInMinutes: minutes });
     chrome.storage.sync.set({ minutes: minutes });
+    window.close();
 }
 
 function clearAlarm() {
-    clearInterval(getChangeTime)
+    clearInterval(time)
     chrome.browserAction.setBadgeText({ text: '' });
     chrome.alarms.clearAll();
+    window.close();
 }
 
 document.getElementById('startButton').addEventListener('click', setAlarm)
