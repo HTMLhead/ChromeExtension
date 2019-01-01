14aa198b8d96cac4a6e4fd676f3b991aa9c129ad
Head <mrdoo309@gmail.com>
2019-01-01 20:37:48 +0900

    피드백 받은대로 수정
    isNumber 함수가 실행되었을때 true가 나와야 그것이 숫자라는 의미인데, 잘못 사용하고 있었음.
    그러므로 isNotNumber가 함수 이름으로 적합하다 여겨져 변경, clearInterval메서드를 제거함
    문자열을 한 변수에 선언해여 저장한 뒤에 사용하도록 변경, 모두 class를 사용한 식으로 변경
    하였음.

--- IHurtMyBack.js
+++ IHurtMyBack.js
@@ -1,37 +1,96 @@
-'use strict';
+class App {
+  constructor() {}
 
-function isNumber(letter) {
-    if (isNaN(letter)) {
-        return true
-    }
-}
-function setAlarm() {
-    let minutes = Number(document.getElementById('time').value)
-    if (isNumber(minutes)) return alert('숫자만 입력해 주세요.')
-    chrome.browserAction.setBadgeText({ text: `${minutes}` })
-    chrome.alarms.create({ delayInMinutes: minutes }); 
+  setAlarm() {
+    let minutes = Number(document.getElementById("time").value);
+    if (app.isNotNumber(minutes)) return alert("숫자만 입력해 주세요.");
+    chrome.browserAction.setBadgeText({ text: `${minutes}` });
+    chrome.alarms.create({ delayInMinutes: minutes });
     window.close();
-}
-function clearAlarm() {
-    clearInterval(time)
-    chrome.browserAction.setBadgeText({ text: '' });
+  }
+  isNotNumber(letter) {
+    if (isNaN(letter)) {
+      return true;
+    }
+    return false;
+  }
+  clearAlarm() {
+    chrome.browserAction.setBadgeText({ text: "" });
     chrome.alarms.clearAll();
     window.close();
-}
-function alertManual() {
-    alert(`익스텐션의 목적
+  }
+  alertManual() {
+    const explain = `익스텐션의 목적
 -계속 앉아 작업만 한다면 허리에도, 눈에도 악영향을 끼칩니다.
 -분대의 시간을 정해서 한번씩 일어나서 스트레칭을 해줍시다.
-
+		
 사용법
 -1. '분 입력' 이 적혀있는 빈칸에, 원하는 분대의 시간을 입력합니다.
 -2. 사람이 달리기 시작을 준비하고 있는 자세의 start버튼을 클릭합니다.
 -3. 클릭하시면, 창이닫히며 익스텐션의 위치에 정하신 시간이 표시됩니다.(분)
 -4. 시간을 초기화하고 싶으실때는 delete버튼을 누르시고 처음부터 다시 진행하시면 됩니다.
+		
+제작자 : head, 이모티콘 출처 : https://thenounproject.com/`;
 
-제작자 : head, 이모티콘 출처 : https://thenounproject.com/`)
+    alert(explain);
+  }
 }
 
-document.getElementById('manual').addEventListener('click', alertManual)
-document.getElementById('startButton').addEventListener('click', setAlarm)
-document.getElementById('clearButton').addEventListener('click', clearAlarm)
+class ClickEvent {
+  constructor(app) {
+    this.app = app;
+  }
+
+  add() {
+    document
+      .getElementById("manual")
+      .addEventListener("click", this.app.alertManual);
+    document
+      .getElementById("startButton")
+      .addEventListener("click", this.app.setAlarm);
+    document
+      .getElementById("clearButton")
+      .addEventListener("click", this.app.clearAlarm);
+  }
+}
+
+const app = new App();
+const clickEvent = new ClickEvent(app);
+clickEvent.add();
+// ------------------------------------------------------------------------------
+// function isNotNumber(letter) {
+//   if (isNaN(letter)) {
+//     return true;
+//   }
+//   return false;
+// }
+// function setAlarm() {
+//   let minutes = Number(document.getElementById("time").value);
+//   if (isNotNumber(minutes)) return alert("숫자만 입력해 주세요.");
+//   chrome.browserAction.setBadgeText({ text: `${minutes}` });
+//   chrome.alarms.create({ delayInMinutes: minutes });
+//   window.close();
+// }
+// function clearAlarm() {
+//   chrome.browserAction.setBadgeText({ text: "" });
+//   chrome.alarms.clearAll();
+//   window.close();
+// }
+// function alertManual() {
+//   const explain = `익스텐션의 목적
+// 	-계속 앉아 작업만 한다면 허리에도, 눈에도 악영향을 끼칩니다.
+// 	-분대의 시간을 정해서 한번씩 일어나서 스트레칭을 해줍시다.
+
+// 	사용법
+// 	-1. '분 입력' 이 적혀있는 빈칸에, 원하는 분대의 시간을 입력합니다.
+// 	-2. 사람이 달리기 시작을 준비하고 있는 자세의 start버튼을 클릭합니다.
+// 	-3. 클릭하시면, 창이닫히며 익스텐션의 위치에 정하신 시간이 표시됩니다.(분)
+// 	-4. 시간을 초기화하고 싶으실때는 delete버튼을 누르시고 처음부터 다시 진행하시면 됩니다.
+
+// 	제작자 : head, 이모티콘 출처 : https://thenounproject.com/`;
+//   alert(explain);
+// }
+
+// document.getElementById("manual").addEventListener("click", alertManual);
+// document.getElementById("startButton").addEventListener("click", setAlarm);
+// document.getElementById("clearButton").addEventListener("click", clearAlarm);
