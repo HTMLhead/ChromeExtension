204206e792c395cfff1dcefe5f1d43b0a5f771a7
Head <mrdoo309@gmail.com>
2019-01-28 16:45:23 +0900

    전체적 구조완성 및 엔터키 동작가능

--- JS/IHurtMyBack.app.js
+++ JS/IHurtMyBack.app.js
@@ -1,5 +1,11 @@
-import { } from './IHurtMyBack.IHMB.js'
+import { IHMB } from "./IHurtMyBack.IHMB.js";
+import { Event } from "./IHurtMyBack.event.js";
 
 document.addEventListener("DOMContentLoaded", _ => {
-
-})
+  const Ihmb = new IHMB();
+  new Event(Ihmb, 
+    document.getElementById("explane"),
+    document.getElementById("startButton"),
+    document.getElementById("clearButton"),
+    document.getElementById("time"));
+});
--- JS/IHurtMyBack.event.js
+++ JS/IHurtMyBack.event.js
@@ -1,9 +1,10 @@
 class Event {
-  constructor(IHMB, explaneBtn, startBtn, endBtn) {
-    this.ihmb = IHMB;
-    this.explaneBtn = explaneBtn;
-    this.startBtn = startBtn;
-    this.endBtn = endBtn;
+  constructor(IHMB, explaneBtn, startBtn, endBtn, input) {
+    this.ihmb = IHMB
+    this.explaneBtn = explaneBtn
+    this.startBtn = startBtn
+    this.endBtn = endBtn
+    this.input = input
     this.init();
   }
 
@@ -11,5 +12,8 @@ class Event {
     this.explaneBtn.addEventListener('click', this.ihmb.alertManual.bind(this));
     this.startBtn.addEventListener('click', this.ihmb.setAlarm.bind(this));
     this.endBtn.addEventListener('click', this.ihmb.clearAlarm.bind(this));
-  }
+    this.input.addEventListener('keypress', this.ihmb.enterPress.bind(this));
+  } 
 }
+
+export { Event }
