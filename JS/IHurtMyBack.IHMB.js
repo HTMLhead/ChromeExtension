e48ac36ad43dc275295779e0d4be55450318a107
Head <mrdoo309@gmail.com>
2019-01-28 16:07:55 +0900

    IHMB파일 완성
    모든 기능들이 들어있는 js파일 작성

--- JS/IHurtMyBack.IHMB.js
+++ JS/IHurtMyBack.IHMB.js
@@ -1,6 +1,5 @@
-class App {
+class IHMB {
   constructor() {}
-
   setAlarm() {
     let time = document.getElementById("time").value;
     if (time === "") return alert(`값을 입력해주세요.`);
@@ -25,34 +24,10 @@ class App {
 		
 사용법
 -1. '분 입력' 이 적혀있는 빈칸에, 원하는 분대의 시간을 입력합니다.
--2. 사람이 달리기 시작을 준비하고 있는 자세의 start버튼을 클릭합니다.
+-2. 시작 버튼을 클릭합니다.
 -3. 클릭하시면, 창이닫히며 익스텐션의 위치에 정하신 시간이 표시됩니다.(분)
--4. 시간을 초기화하고 싶으실때는 delete버튼을 누르시고 처음부터 다시 진행하시면 됩니다.
-		
-제작자 : head, 이모티콘 출처 : https://thenounproject.com/`;
+-4. 시간을 초기화하고 싶으실때는 제거 버튼을 누르시고 처음부터 다시 진행하시면 됩니다.`;
 
     alert(explain);
   }
 }
-
-class ClickEvent {
-  constructor(app) {
-    this.app = app;
-  }
-
-  add() {
-    document
-      .getElementById("manual")
-      .addEventListener("click", this.app.alertManual);
-    document
-      .getElementById("startButton")
-      .addEventListener("click", this.app.setAlarm);
-    document
-      .getElementById("clearButton")
-      .addEventListener("click", this.app.clearAlarm);
-  }
-}
-
-const app = new App();
-const clickEvent = new ClickEvent(app);
-clickEvent.add();
