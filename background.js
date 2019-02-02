dd056593cd0f9864e44a7ee15cb6f1755e7d8001
Head <mrdoo309@gmail.com>
2019-02-02 15:15:53 +0900

    reset

--- background.js
+++ background.js
@@ -1,21 +1,18 @@
-console.log('hi');
-
 chrome.alarms.onAlarm.addListener(function() {
-  chrome.browserAction.setBadgeText({text: ''});
+  chrome.browserAction.setBadgeText({ text: "" });
   chrome.notifications.create({
-      type:     'basic',
-      iconUrl:  'extensionImage/IHurtMyBack.png',
-      title:    '스트레칭하세요!',
-      message:  ' ',
-      buttons: [
-        {title: '타이틀에 마우스를 계속 대고있어보세요.'}
-      ],
-      priority: 0});
+    type: "basic",
+    iconUrl: "extensionImage/IHurtMyBack.png",
+    title: "스트레칭하세요!",
+    message: " ",
+    buttons: [{ title: "타이틀에 마우스를 계속 대고있어보세요." }],
+    priority: 0
+  });
 });
 
 chrome.notifications.onButtonClicked.addListener(function() {
-  chrome.storage.sync.get(['minutes'], function(item) {
-    chrome.browserAction.setBadgeText({text: item.minutes});
-    chrome.alarms.create({delayInMinutes: item.minutes});
+  chrome.storage.sync.get(["minutes"], function(item) {
+    chrome.browserAction.setBadgeText({ text: item.minutes });
+    chrome.alarms.create({ delayInMinutes: item.minutes });
   });
 });
