81597c4d5acc47a48e9e6107371c51113e002c3a
HTMLhead <mrdoo309@gmail.com>
2019-02-06 19:59:33 -0800

    add all file

---
+++ back.js
@@ -0,0 +1,18 @@
+chrome.alarms.onAlarm.addListener(function() {
+  chrome.browserAction.setBadgeText({ text: "" });
+  chrome.notifications.create({
+    type: "basic",
+    iconUrl: "extensionImage/IHurtMyBack.png",
+    title: "스트레칭할 시간입니다!",
+    message: " ",
+    buttons: [{ title: "찾아보세요 이스터에그!" }],
+    priority: 0
+  });
+});
+
+chrome.notifications.onButtonClicked.addListener(function() {
+  chrome.storage.sync.get(["minutes"], function(item) {
+    chrome.browserAction.setBadgeText({ text: `${item.minutes}` });
+    chrome.alarms.create({ delayInMinutes: item.minutes });
+  });
+});
---
+++ util.js
@@ -0,0 +1,5 @@
+function qs(part, value) {
+  return part.querySelector(value)
+}
+
+export { qs }
