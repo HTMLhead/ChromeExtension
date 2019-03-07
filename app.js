a812a115e3a064aa943329234608df90264a1ad3
HTMLhead <mrdoo309@gmail.com>
2019-03-07 12:24:08 +0900

    Refactor: alarm기능을 import로 가져오도록 수정

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import { qs } from './util.js';
+import { Alarm } from './setAlarm.js';
 
 document.addEventListener('DOMContentLoaded', () => {
+  const alarm = new Alarm()
   const eventNode = qs(document, '.event');
   qs(eventNode, '.test').addEventListener('click', setAlarm)
   qs(eventNode, '.twentyMin').addEventListener('click', setAlarm);
