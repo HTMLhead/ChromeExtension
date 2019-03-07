e3e0ad8a47e932f01dc6f83432f1357cf8f402ac
HTMLhead <mrdoo309@gmail.com>
2019-03-07 12:36:18 +0900

    Style: 코드 정렬, 세미콜론 추가

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ import { qs } from './util.js';
 import { Alarm } from './setAlarm.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const alarm = new Alarm()
+  const alarm = new Alarm();
   const eventNode = qs(document, '.event');
-  qs(eventNode, '.test').addEventListener('click', alarm.set)
+  qs(eventNode, '.test').addEventListener('click', alarm.set);
   qs(eventNode, '.twentyMin').addEventListener('click', alarm.set);
-  qs(eventNode, '.fortyMin').addEventListener('click', alarm.set)
-  qs(eventNode, '.sixytMin').addEventListener('click', alarm.set)
-  qs(eventNode, '.explane').addEventListener('click', alarm.addExplane)
-  qs(eventNode, '.clearButton').addEventListener('click', alarm.clear)
-})
+  qs(eventNode, '.fortyMin').addEventListener('click', alarm.set);
+  qs(eventNode, '.sixytMin').addEventListener('click', alarm.set);
+  qs(eventNode, '.explane').addEventListener('click', alarm.addExplane);
+  qs(eventNode, '.clearButton').addEventListener('click', alarm.clear);
+});
--- setAlarm.js
+++ setAlarm.js
@@ -3,22 +3,22 @@ import { qs } from './util.js';
 class Alarm {
   set(event) {
     let minutes = parseFloat(event.target.innerText);
-    chrome.browserAction.setBadgeText({text: `${minutes}`});
-    chrome.alarms.create({delayInMinutes: minutes});
-    chrome.storage.sync.set({minutes: minutes});
+    chrome.browserAction.setBadgeText({ text: `${minutes}` });
+    chrome.alarms.create({ delayInMinutes: minutes });
+    chrome.storage.sync.set({ minutes: minutes });
     window.close();
   }
-  
+
   clear() {
-    chrome.browserAction.setBadgeText({text: ''});
+    chrome.browserAction.setBadgeText({ text: '' });
     chrome.alarms.clearAll();
     window.close();
   }
-  
+
   addExplane() {
     const explaneContainer = qs(document, '.explane-container');
     explaneContainer.classList.toggle('explane-open');
   }
 }
 
-export { Alarm }
+export { Alarm };
